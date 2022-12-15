d98d155851c3ece5e1f61affb2a486e5c6ed966c
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-15 20:40:59 -0300

    chore(enviroment): configuração bd & auth Firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCtH_HSWiHSGdojJWdV_-44jGvJyA28-4U",
+    authDomain: "vue-todo-231b9.firebaseapp.com",
+    databaseURL: "https://vue-todo-231b9-default-rtdb.firebaseio.com",
+    projectId: "vue-todo-231b9",
+    storageBucket: "vue-todo-231b9.appspot.com",
+    messagingSenderId: "1084120166573",
+    appId: "1:1084120166573:web:9cf89750daac78ea719624",
+    measurementId: "G-JWHLQJENX3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+createApp(App).mount("#app");
